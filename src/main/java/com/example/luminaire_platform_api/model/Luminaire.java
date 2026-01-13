d675553d73e5de82777cc3a6e0e2bd70953425a6
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2026-01-13 17:35:36 +0100

    Add luminaire entity

--- src/main/java/com/example/luminaire_platform_api/model/Luminaire.java
+++ src/main/java/com/example/luminaire_platform_api/model/Luminaire.java
@@ -1,4 +1,58 @@
 package com.example.luminaire_platform_api.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "luminaires")
 public class Luminaire {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String type;
+
+    // private String rating;
+
+    @ManyToOne
+    @JoinColumn(name= "manufacturer_id")
+    private Manufacturer manufacturer;
+
+    public Luminaire(){}
+    public Luminaire(String name, String type){
+        this.name= name;
+        this.type= type;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 }
