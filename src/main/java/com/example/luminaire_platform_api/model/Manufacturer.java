118c1edf6f28c768d0066b5efe70c9b07d804b73
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2026-01-13 17:26:49 +0100

    create manufacturer entity

--- src/main/java/com/example/luminaire_platform_api/model/Manufacturer.java
+++ src/main/java/com/example/luminaire_platform_api/model/Manufacturer.java
@@ -1,17 +1,61 @@
 package com.example.luminaire_platform_api.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name= "manufactures")
-public class Manufacture {
+public class Manufacturer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @OneToMany(mappedBy = "manufacture" )
-    private List<Luminaire> luminaires = new ArrayList<>();
+    private String contactEmail;
+    @OneToMany(mappedBy = "manufacturer" )
+    @JsonBackReference
+    private Set<Luminaire> luminaires;
+
+
+    public Manufacturer(){
+
+    }
+    public Manufacturer(String name, String contactEmail ){
+        this.name=name;
+        this.contactEmail= contactEmail;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public Set<Luminaire> getLuminaires() {
+        return luminaires;
+    }
+
+    public void setLuminaires(Set<Luminaire> luminaires) {
+        this.luminaires = luminaires;
+    }
 }
+
