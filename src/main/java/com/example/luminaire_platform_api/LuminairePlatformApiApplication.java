cc41b7915f7add71e4ec7467c749a4fe1cbd7b61
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2026-01-13 13:54:50 +0100

    Initialise spring Boot project setup

---
+++ src/main/java/com/example/luminaire_platform_api/LuminairePlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.luminaire_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminairePlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuminairePlatformApiApplication.class, args);
+	}
+
+}
