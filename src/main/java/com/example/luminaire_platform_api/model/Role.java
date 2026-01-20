c7a13db4bd212ef3797d450ff4e2e65bc6087f5c
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2026-01-20 15:40:18 +0100

    create Role entity

--- src/main/java/com/example/luminaire_platform_api/model/Role.java
+++ src/main/java/com/example/luminaire_platform_api/model/Role.java
@@ -1,4 +1,31 @@
 package com.example.luminaire_platform_api.model;
 
+import jakarta.persistence.*;
+
+@Table(name = "roles")
+@Entity
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
