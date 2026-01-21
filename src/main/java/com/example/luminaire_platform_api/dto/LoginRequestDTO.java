0dfd96ec7868b89b6ec61058a0de65c961484cf8
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2026-01-21 15:00:55 +0100

    make login request and response DTO

--- src/main/java/com/example/luminaire_platform_api/dto/LoginRequestDTO.java
+++ src/main/java/com/example/luminaire_platform_api/dto/LoginRequestDTO.java
@@ -1,4 +1,26 @@
 package com.example.luminaire_platform_api.dto;
 
+
 public class LoginRequestDTO {
+    String name;
+    String password;
+
+    public LoginRequestDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/example/luminaire_platform_api/dto/LoginResponseDTO.java
+++ src/main/java/com/example/luminaire_platform_api/dto/LoginResponseDTO.java
@@ -1,4 +1,13 @@
 package com.example.luminaire_platform_api.dto;
 
 public class LoginResponseDTO {
+    private final String token;
+
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
 }
